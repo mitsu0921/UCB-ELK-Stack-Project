6795a47bb0d487484ee3d008b687838d96dbfe0b
Brian Chew <66648514+mitsu0921@users.noreply.github.com>
2021-09-21 12:05:22 -0700

    Create roulette_dealer_finder_by_time_and_game.sh

---
+++ Linux/roulette_dealer_finder_by_time_and_game.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Specify a time:(XX:XX:XX PM)" 
+read -r Num1
+echo "Specify a date (XXXX):"
+read -r Num2
+echo "Specify a game being played:" 
+read -r Num3
+
+echo $Num1 $Num2 $Num3 
+
